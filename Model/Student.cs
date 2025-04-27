1c5711c9c0f4c53948e1985fba2341d81e8dc322
VA-Pavlov <vap.uspeh@gmail.com>
2025-04-27 19:12:40 +0300

    Добавлен запрос на создание БД, а также классы-модели для выгрузки данных из БД. Добавлена форма регистрации.

---
+++ Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPKPApp.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string SecondName { get; set; }
+        public Group GroupNumber { get; set; }
+    }
+}
---
+++ Model/StudentWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPKPApp.Model
+{
+    public class StudentWork
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateOfDefense { get; set; }
+        public TypeOfStudentWork TypeOfWork {  get; set; }
+        public Teacher Teacher { get; set; }
+        public Student Student { get; set; }
+    }
+}
