3f2ace126f5479e76797940f5533171060ca66d8
VA-Pavlov <vap.uspeh@gmail.com>
2025-04-28 08:52:00 +0300

    Добавлен метод, закрывающий окно регистрации

--- View/RegistrationWindow.xaml.cs
+++ View/RegistrationWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace DPKPApp.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
